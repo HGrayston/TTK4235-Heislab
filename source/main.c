6874cc334b496b50956b61ff044392c6d00f173b
Hans Gunnar Grayston <88401310+HGrayston@users.noreply.github.com>
2023-03-14 20:39:42 +0100

    Update main.c

--- source/main.c
+++ source/main.c
@@ -14,31 +14,51 @@ int main(){
 
 
 
-
-
   while(1)
     {
         while (heis->prioriteringsko[0] == 0)
         {
+
             sjekketasjeknapp(heis);
         }
-        feilsideavretn(heis);
-        oppdatertarget(heis);
-       
         
+//        feilsideavretn(heis);
+        kjortilbestilling(heis);
         
-        getRetning(heis);
+// Nå skal heisen stå stille i etasjen den har fått bestilling fra
+// Her vil vi kjøre delay + oppdatere target
+        ventpabestilling(heis);
+
+        if(heis->targetFloor != heis->currentFloor){
+            startHeis(heis);
+        }
         startHeis(heis);
+
         while (heis->targetFloor != heis->currentFloor)
         {
-            getFloor(heis);
+            
             sjekkomStopp(heis); // Må inneholde funksjon for oppdatering av target ++ logikk for når mellom etasjer
-            stoppknapp(heis);
+     //       stoppknapp(heis);
             sjekketasjeknapp(heis);
             oppdatertarget(heis);
+            getFloor(heis);
         }
+        stoppHeis();
+        fjernfrako(heis);
+
     }
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
 
