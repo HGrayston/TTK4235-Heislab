d5f35c524dc3f9f430c74fb543208d01c6d0ba3e
PerMartinH <127187886+PerMartinH@users.noreply.github.com>
2023-03-14 18:39:39 +0100

    Update main.c

--- source/main.c
+++ source/main.c
@@ -48,7 +48,6 @@ int main(){
 }
 
 
-//skal kjøres hele tiden - sjekketasjeknapp
 
 
 
@@ -59,42 +58,3 @@ int main(){
 
 
 
-
-
-
-
-
-  while(1)
-    {
-        while (heis->prioriteringsko[0] == 0)
-        {
-            sjekketasjeknapp(heis);
-        }
-        
-//        feilsideavretn(heis);
-        kjortilbestilling(heis);
-        
-// Nå skal heisen stå stille i etasjen den har fått bestilling fra
-// Her vil vi kjøre delay + oppdatere target
-        ventpabestilling(heis);
-
-
-        startHeis(heis);
-
-        while (heis->targetFloor != heis->currentFloor)
-        {
-            oppdatertarget(heis);
-            sjekkomStopp(heis); 
-     //       Må inneholde funksjon for oppdatering av target ++ logikk for når mellom etasjer
-     //       stoppknapp(heis);
-     //       sjekketasjeknapp(heis);
-     //       oppdatertarget(heis);
-            getFloor(heis);
-        }
-        stoppHeis();
-        fjernfrako(heis);
-
-    }
-
-    return 0;
-}
